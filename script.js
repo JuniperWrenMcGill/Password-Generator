f821823bf2fdfe21ce2429aa43c006b11b041e81
Juniper <juniperwrenarts@gmail.com>
2023-12-18 16:36:41 -0500

    reset code to starter code

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var upperLetter ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerLetter = "abcdefghijklmnopqrstuvwxyz";
-var num = "123456789";
-var specialcar = "!?~*$@"
 
-// // Write password to the #password input
+// Write password to the #password input
 function writePassword() {
-  // function myFunction() {
-  var txt;
-  if (confirm("Do you want to include lowercase letters in password? ")) {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
-}
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-  // passwordText.value = password;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
