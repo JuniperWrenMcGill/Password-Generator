86ed3e4488aed707ab49c91b0b65d893bf820ace
Juniper <juniperwrenarts@gmail.com>
2023-12-21 11:23:51 -0500

    added var + notes to guide me

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var pass = '';
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var special = '@#!?~-';
+var numberString = '';
 
-// Write password to the #password input
+// prompted for the length of the password with the choice of length = 8 - 128 characters
+
+// promted to add lowercase characters
+
+// promted to add uppercase characters
+
+// prompted to add special characters
+
+// prompter to add numbers 
+
+
+// Write password to the #password input aka the password is displayed on the page 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
